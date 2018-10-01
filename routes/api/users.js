baf4c9763bd0f33a7a98dc76498a0783fa64fbc9
DavidGuben <davidjg6@gmail.com>
2018-10-01 11:25:47 -0400

    added User model for mongoose to use to generate Users and saveto the mongo database, bcryptjs encryption for password. post request to database created for registering new users (/routes/api/users), added gravatar for generating avatars

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrpyt = require('bcryptjs');
 
+// Load User model
+const User = require('../../models/User');
+
+// @route GET api/users/test
+// @access Public
 router.get('/test', (req, res) => res.json({msg: "Users works"}));
+
+// @route GET api/users/register
+// @access Public
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(user) {
+                return res.status(400).json({email: 'Email already exists'});
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // Size
+                    r: 'pg', // Rating
+                    d: 'mm'  // Default
+                });
+
+                // Create a new user using the mongoose User model
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar: avatar,
+                    password: req.body.password
+                });
+
+                // Encrypt password: Generate salt w/ bcryptjs, hash password w/ salt, set password to hash.
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                    })
+                })
+            }
+        })
+});
+
 
 module.exports = router;
